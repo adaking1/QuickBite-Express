5f59dd26f0eff446400cd8c3804a6d29621ed2cd
adaking1 <137830553+adaking1@users.noreply.github.com>
2024-01-04 20:18:23 -0500

    completed signup form

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -33,13 +33,58 @@ const SignupForm = () => {
         }
         setFormData({ username: '', email: '', password: '' });
     };
-};
 
 return (
     <>
     <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        
-
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+        Unable to signup!
+        </Alert>
+        <Form.Group className='mb-3'>
+            <Form.Label htmlFor='username'>Username</Form.Label>
+            <Form.Control
+                type='text'
+                placeholder='username'
+                name='username'
+                onChange={handleInputChange}
+                value={formData.username}
+                required
+            />
+            <Form.Control.Feedback type='invalid'>Username required!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className='mb-3'>
+            <Form.Label htmlFor='email'>Email</Form.Label>
+            <Form.Control
+                type='email'
+                placeholder='email address'
+                name='email'
+                onChange={handleInputChange}
+                value={formData.email}
+                required
+            />
+            <Form.Control.Feedback type='invalid'>Email required!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className='mb-3'>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <Form.Control
+                type='password'
+                placeholder='password'
+                name='usernpasswordame'
+                onChange={handleInputChange}
+                value={formData.password}
+                required
+            />
+            <Form.Control.Feedback type='invalid'>Username required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+        disabled={!(formData.username && formData.email && formData.password)}
+        type='submit'
+        variant='success'>
+        Submit
+        </Button>
     </Form>
     </>
-)
+    );
+};
+
+export default SignupForm;
