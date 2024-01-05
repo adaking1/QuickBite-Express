d3739e08752c5f6027b8123a3d209a2a8fe25939
adaking1 <137830553+adaking1@users.noreply.github.com>
2024-01-05 12:04:02 -0500

    started homepage logic

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,5 +2,19 @@ import { useState, useEffect } from 'react';
 import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { saveOrderIds, getSavedOrders } from '../utils/localStorage';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { SAVE_ORDER } from '../utils/mutations';
+import { GET_FOOD } from '../utils/queries';
+
+const SearchFood = () => {
+    const [searchedFood, setSearchedFood] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+    const [savedOrderIds, setSavedOrderIds] = useState(getSavedOrders());
+    const [saveOrder, { error }] = useMutation(SAVE_ORDER);
+    const [getFood, { error, data }] = useQuery(GET_FOOD);
+
+    useEffect(() => {
+        return () => saveOrder(savedOrderIds);
+    });
+
+};
