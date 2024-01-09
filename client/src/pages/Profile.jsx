f1046acad4de2245e0c482dc04991f70ca272c28
adaking1 <137830553+adaking1@users.noreply.github.com>
2024-01-08 21:14:34 -0500

    added to profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
-import { removeOrderId } from '../utils/localStorage';
+import { removeRestaurantId } from '../utils/localStorage';
 import { GET_ME } from '../utils/queries';
 import { UPDATE_EMAIL, UPDATE_PASSWORD, UPDATE_USERNAME, REMOVE_RESTAURANT } from '../utils/mutations';
 
@@ -29,12 +29,77 @@ const Profile = () => {
             const { data } = await deleteRestaurant({
                 variables: { restaurantId }
             });
-            remove
+            removeRestaurantId(restaurantId);
+            localStorage.setItem('saved_restaurants', restaurantId);
         }
         catch (err) {
             console.error(err);
         }
     };
+
+    const handleUpdateEmail = async (event) => {
+        event.preventDefault();
+        const newEmail = event.target.value;
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+        if (!token) {
+            return false;
+        }
+        try {
+            const { data } = await updateEmail({
+                variables: { newEmail }
+            });
+            
+        }
+        catch (err) {
+            console.err(err);
+        }
+    };
+
+    const handleUpdateUsername = async (event) => {
+        event.preventDefault();
+        const newUsername = event.target.value;
+        const token = Auth.loggedIn ? Auth.getToken() : null;
+        if (!token) {
+            return false;
+        }
+        try {
+            const { data } = await updateUsername({
+                variables: { newUsername }
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleUpdatePassword = async (event) => {
+        event.preventDefault();
+        const newPassword = event.target.value;
+        const token = Auth.loggedIn ? Auth.getToken() : null;
+        if (!token) {
+            return false;
+        }
+        try {
+            const { data } = await updatePassword({
+                variables: { newPassword }
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+    if (loading) {
+        return <h2>Loading...</h2>
+    };
+
+    return (
+        <>
+        <div fluid='true' className='text-light bg-dark p-5'>
+            
+        </div>
+        </>
+    );
 };
 
 export default Profile;
