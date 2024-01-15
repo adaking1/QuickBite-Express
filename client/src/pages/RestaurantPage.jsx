74b70c0d1d5ac26c3196c75a8cb21225434b7189
adaking1 <137830553+adaking1@users.noreply.github.com>
2024-01-15 09:38:49 -0500

    added mutation import to restaurant page

--- client/src/pages/RestaurantPage.jsx
+++ client/src/pages/RestaurantPage.jsx
@@ -3,6 +3,7 @@ import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { GET_RESTAURANT } from '../utils/queries';
+import { SAVE_RESTAURANT } from '../utils/mutations';
 
 const RestaurantPage = (searchInput) => {
     
